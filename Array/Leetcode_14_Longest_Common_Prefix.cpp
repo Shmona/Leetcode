67fb0af126c2f6ce55f65d63a2fb375fd8e9c5e2
Mona Shree <smona353@gmail.com>
2024-01-20 21:36:43 +0530

    added on 20th Jan

--- Array/Leetcode_14_Longest_Common_Prefix.cpp
+++ Array/Leetcode_14_Longest_Common_Prefix.cpp
@@ -3,10 +3,14 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 
-
 ### Example:
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
+    Input: strs = ["flower","flow","flight"]
+    Output: "fl"
+
+Example 2:
+    Input: strs = ["dog","racecar","car"]
+    Output: ""
+    Explanation: There is no common prefix among the input strings.
 
 ### Constraints:
     1 <= strs.length <= 200
