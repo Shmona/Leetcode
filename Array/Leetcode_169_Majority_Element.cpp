2638336fbf95faca71b6fb61786af0b1a631ced8
Mona Shree <smona353@gmail.com>
2024-02-04 15:43:41 +0530

    Added on 3rd Feb 2024

--- Array/Leetcode_169_Majority_Element.cpp
+++ Array/Leetcode_169_Majority_Element.cpp
@@ -76,3 +76,13 @@ public:
         return ans;   
     }
 };
+
+//Another Approach : using Sorting
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return nums[n/2];
+    }
+};
