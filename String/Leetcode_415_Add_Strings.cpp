46f5f2eae9d4eac7906028a48c8ecb4fa701abf2
Mona Shree <smona353@gmail.com>
2024-02-19 23:12:15 +0530

    Added on 19th feb 2024

---
+++ String/Leetcode_415_Add_Strings.cpp
@@ -0,0 +1,49 @@
+/*
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). 
+You must also not convert the inputs to integers directly.
+
+Example 1:
+    Input: num1 = "11", num2 = "123"
+    Output: "134"
+
+Example 2:
+    Input: num1 = "456", num2 = "77"
+    Output: "533"
+
+Example 3:
+    Input: num1 = "0", num2 = "0"
+    Output: "0"
+
+###Constraints:
+    1 <= num1.length, num2.length <= 10^4
+    num1 and num2 consist of only digits.
+    num1 and num2 don't have any leading zeros except for the zero itself.
+
+*/
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int l1 = num1.length() - 1;
+        int l2 = num2.length() - 1 ;
+        string res;
+        int temp = 0, carry = 0;
+        while (l1 >= 0 || l2 >= 0 || carry > 0)
+        {
+            int d1 = (l1 >= 0) ? (num1[l1] - '0') : 0;
+            int d2 = (l2 >= 0) ? (num2[l2] - '0') : 0;
+
+            temp = d1 + d2 + carry;
+            carry = temp/10;
+            temp = temp%10;
+            res.insert(res.begin(), (temp + '0'));
+            
+            if (l1 >= 0)
+                l1--;
+            if (l2 >= 0)
+                l2--;
+        }
+        return res;
+    }
+};
