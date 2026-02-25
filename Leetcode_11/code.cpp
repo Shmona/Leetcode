d611b38005027237dc73e706b3100d156fbf5c6d
Shmona <95846620+Shmona@users.noreply.github.com>
2026-02-25 21:58:37 +0530

    Update code.cpp

--- Leetcode_11/code.cpp
+++ Leetcode_11/code.cpp
@@ -6,85 +6,38 @@ using namespace std;
 
 class Solution {
 public:
-    // Approach 1 : Two Pointer 
-    int maxArea(vector<int>&height) 
-    {
-        int left = 0;
-        int right = height.size() - 1;
+    int maxArea(vector<int>& height) {
+        int n = height.size();
 
-        int Area = 0;
-        while (right > left)
+        if (n < 2)
+            return 0;
+        
+        int left = 0, right = n - 1;
+        int res = 0;
+
+        while (left < right)
         {
-            int temp = min(height[right], height[left]) *(right - left);
-            Area = max(temp, Area);
-
+            int area = (right - left) * min(height[left],height[right]);
             if (height[left] < height[right])
                 left++;
-            else
+            else if (height[left] > height[right])
                 right--;
-
-        }
-        return Area; 
-    }
-
-    // Approach 2 : Greedy algo 
-    int maxArea2(vector<int>&height) {
-        int left = 0;
-        int right = height.size() - 1;
-
-        int Area = 0;
-        while (right > left)
-        {
-            int temp ;
-            if(height[right] > height[left])
+            else 
             {
-                temp = height[left] *(right - left);
                 left++;
-            }
-            else if (height[right] < height[left])
-            {
-                temp = height[right] *(right - left);
                 right--;
             }
-            else
-            {
-                temp = height[right] *(right - left);
-                right--;
-                left++;
-            }
-            Area = max(temp, Area);
+            res =  max(res, area);
         }
-        return Area; 
+        return res;
     }
 };
 
 int main()
 {
+    vector<int> v1 = {1,8,6,2,5,4,8,3,7};
     Solution sol;
-    vector<int> v ={1,8,6,2,5,4,8,3,7};   //expect 49
-    cout << "Approach 1 :  Two Pointer :   "<< sol.maxArea(v) << endl;
-    cout << "Approach 2 :  Greedy algo  : "<< sol.maxArea2(v) << endl;
 
-    
-    vector<int> a = {1, 1};                  // expect 1
-    cout << "Approach 1 :  Two Pointer :   "<< sol.maxArea(a) << endl;
-    cout <<"Approach 2 :  Greedy algo  : "<< sol.maxArea2(a) << endl;
-
-    vector<int> b = {4, 3, 2, 1, 4};         // expect 16
-    cout <<"Approach 1 :  Two Pointer :   "<< sol.maxArea(b) << endl;
-    cout <<"Approach 2 :  Greedy algo  : "<< sol.maxArea2(b) << endl;
-
-    vector<int> c = {1, 2, 1};               // expect 2
-    cout << "Approach 1 :  Two Pointer :   "<<sol.maxArea(c) << endl;
-    cout <<"Approach 2 :  Greedy algo  : "<< sol.maxArea2(c) << endl;
-
-    vector<int> d = {};                      // expect 0
-    cout <<"Approach 1 :  Two Pointer :   "<< sol.maxArea(d) << endl;
-    cout <<"Approach 2 :  Greedy algo  : "<< sol.maxArea2(d) << endl;
-
-    vector<int> e = {5};                     // expect 0
-    cout <<"Approach 1 :  Two Pointer :   "<< sol.maxArea(e) << endl;
-    cout <<"Approach 2 :  Greedy algo  : "<< sol.maxArea2(e) << endl;
-
+    cout << sol.maxArea(v1) << endl;  //expects 49
     return 0;
 }
